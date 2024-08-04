6b5f6b97d3eeeca15a218a71786421f46616d16e
YaqubAliy02 <yaqubaliy02@gmail.com>
2024-08-04 09:24:19 +0500

    Complete Project

--- TodoApp/Models/ToDoItem.cs
+++ TodoApp/Models/ToDoItem.cs
@@ -4,7 +4,19 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = "";
-        public bool IsCompleted { get; set; }
-        public DateTimeOffset DateCompleted { get; set; }
+        private bool _isCompleted;
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            set
+            {
+                _isCompleted = value;
+                if (value)
+                {
+                    DateCompleted = DateTime.UtcNow;
+                }
+            }
+        }
+        public DateTime DateCompleted { get; set; }
     }
 }
